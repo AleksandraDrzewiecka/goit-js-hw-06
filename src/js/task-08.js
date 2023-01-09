0ef49ac245dd979a4943203497fac9786e0fd9b0
AleksandraDrzewiecka <111142458+AleksandraDrzewiecka@users.noreply.github.com>
2023-01-09 09:57:57 +0100

    Update task-08.js
    check

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const formElement = document.querySelector('.login-form');
 
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = formElement.elements.email.value;
+  const password = formElement.elements.password.value;
+
+  if (!email || !password) {
+    alert('Nie wszystkie pola zostaly wypełnione!');
+    return;
+  }
+
+  const loginData = { email, password };
+  console.log(loginData);
+  formElement.reset();
+});
